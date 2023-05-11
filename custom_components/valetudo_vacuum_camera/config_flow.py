cabdf5f62e143132ac747da2b99dec1d2a7a9550
SCA075 <82227818+sca075@users.noreply.github.com>
2023-05-11 19:50:51 +0200

    Add files via upload

--- custom_components/valetudo_vacuum_camera/config_flow.py
+++ custom_components/valetudo_vacuum_camera/config_flow.py
@@ -1,74 +1,75 @@
+import logging
+
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.core import callback
-from homeassistant.const import CONF_NAME
-from homeassistant.core import callback
-from homeassistant.data_entry_flow import AbortFlow, FlowResult
-from homeassistant.exceptions import HomeAssistantError
+from homeassistant.const import CONF_HOST, CONF_NAME, CONF_TOKEN
+from homeassistant.helpers.aiohttp_client import async_create_clientsession
 
-from .const import (
-    DOMAIN,
-    CONF_VACUUM_ENTITY,
-    CONF_VACUUM_MAP
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_VACUUM_ENTITY_ID): str,
+        vol.Required(CONF_VACUUM_CONNECTION_STRING): str,
+        vol.Optional(CONF_NAME, default="Valetudo Vacuum"): str,
+    }
 )
 
 
+async def validate_input(hass, data):
+    session = async_create_clientsession(hass)
+    try:
+        valetudo = await hass.async_add_executor_job(
+            ValetudoAPI, data[CONF_VACUUM_ENTITY_ID], data[CONF_VACUUM_CONNECTION_STRING], session
+        )
+        status = await valetudo.get_status()
+    except CannotConnect:
+        raise CannotConnectError
+    except Unauthorized:
+        raise InvalidAuthError
+    except Exception as ex:  # pylint: disable=broad-except
+        _LOGGER.error("Unexpected exception %s", ex)
+        raise UnknownError
+
+    return {"status": status}
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Valetudo Camera."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
+
     async def async_step_user(self, user_input=None):
-        if user_input is not None:
-            self.vacuum_entity = user_input.get(CONF_VACUUM_ENTITY)
-            self.vacuum_map = user_input.get(CONF_VACUUM_MAP)
+        """Handle the initial step."""
+        if user_input is None:
+            return self._show_form()
 
-            return self.async_create_entry(
-                title=self.vacuum_entity, data=user_input
-            )
+        try:
+            info = await validate_input(self.hass, user_input)
+        except CannotConnectError:
+            return self.async_abort(reason="cannot_connect")
+        except InvalidAuthError:
+            return self.async_abort(reason="invalid_auth")
+        except UnknownError:
+            return self.async_abort(reason="unknown")
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Optional(CONF_VACUUM_ENTITY, default=self.vacuum_entity or ""): str,
-                    vol.Optional(CONF_VACUUM_MAP, default=self.vacuum_map or ""): str,
-                }
-            ),
+        name = user_input[CONF_NAME]
+        host = user_input[CONF_HOST]
+        token = user_input[CONF_TOKEN]
+
+        await self.async_set_unique_id(host)
+        self._abort_if_unique_id_configured()
+
+        return self.async_create_entry(
+            title=name, data={CONF_HOST: host, CONF_TOKEN: token}
         )
 
-class ValetudoCameraFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
-
-    def __init__(self):
-        self.vacuum_entity = ""
-        self.vacuum_map = ""
-
-class ValetudoCameraOptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle a option flow for a config entry."""
-
-    def __init__(self, config_entry):
-        """Initialize the options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        """Manage the options."""
-        if user_input is not None:
-            unique_id = user_input.get(CONF_UNIQUE_ID)
-
-            # Update the unique_id of the config entry.
-            self.hass.config_entries.async_update_entry(
-                self.config_entry, unique_id=unique_id
-            )
-
-            # Return to the previous options screen.
-            return self.async_create_entry(title="", data={})
-
-        # Display the options form.
+    def _show_form(self, errors=None):
+        """Show the form to the user."""
         return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema(
-                {
-                    vol.Optional(
-                        CONF_UNIQUE_ID, default=self.config_entry.unique_id
-                    ): str
-                }
-            ),
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors or {}
         )
