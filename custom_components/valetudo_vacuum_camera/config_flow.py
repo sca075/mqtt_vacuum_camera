fdb0cf950af819f89ba4c8c401bfeff5c852cd1c
SCA075 <82227818+sca075@users.noreply.github.com>
2023-05-12 15:48:43 +0200

    Update config_flow.py

--- custom_components/valetudo_vacuum_camera/config_flow.py
+++ custom_components/valetudo_vacuum_camera/config_flow.py
@@ -5,9 +5,9 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_NAME
 from homeassistant.helpers import config_validation as cv
+from homeassistant.exceptions import ConfigEntryAuthFailed, Unauthorized, UnknownUser
 
-from .const import CONF_VACUUM_CONNECTION_STRING, CONF_VACUUM_ENTITY_ID, DEFAULT_NAME, DOMAIN
-
+from .const import CONF_VACUUM_CONNECTION_STRING, CONF_VACUUM_ENTITY_ID, DEFAULT_NAME, DOMAIN, ICON
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -22,7 +22,7 @@ DATA_SCHEMA = vol.Schema(
 
 async def validate_input(hass, data):
     entity_id = data.get(CONF_VACUUM_ENTITY_ID)
-    mqtt_entity_id = "mqtt.camera_{}_map".format(entity_id.split('.')[1])
+    mqtt_entity_id = data.get(CONF_VACUUM_CONNECTION_STRING)
 
     # Check if vacuum entity exists
     if not hass.states.get(entity_id):
@@ -39,48 +39,22 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Valetudo Camera."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         errors = {}
         if user_input is not None:
             # Check if the vacuum entity exists
-            vacuum_entity = user_input.get(CONF_VACUUM_ENTITY_ID)
-            entity_registry = await self.hass.helpers.entity_registry.async_get_registry()
-            if vacuum_entity not in entity_registry.entities:
-                errors["base"] = "invalid_entity_id"
-            else:
-                # Check if the MQTT image entity exists
-                mqtt_entity_id = f"camera.{user_input[CONF_NAME].lower()}_map"
-                if mqtt_entity_id not in entity_registry.entities:
-                    errors["base"] = "missing_mqtt_entity_id"
-                else:
-                    # Try to validate the input
-                    try:
-                        info = await validate_input(self.hass, user_input)
-                        return self.async_create_entry(
-                            title=user_input[CONF_NAME],
-                            data={
-                                CONF_VACUUM_ENTITY_ID: user_input[CONF_VACUUM_ENTITY_ID],
-                                CONF_VACUUM_CONNECTION_STRING: user_input[CONF_VACUUM_CONNECTION_STRING],
-                            },
-                        )
-                    except CannotConnectError:
-                        errors["base"] = "cannot_connect"
-                    except InvalidAuthError:
-                        errors["base"] = "invalid_auth"
-                    except UnknownError:
-                        errors["base"] = "unknown_error"
+            self.CONF_VACUUM_ENTITY_ID = user_input[vacuum_entity]
+            self.CONF_VACUUM_CONNECTION_STRING = user_input[vacuum_map]
+
+        # Removed as temporally error handling
+        #"error": {
+        #    "cannot_connect": "Could not connect to vacuum. Please check the connection and try again.",
+        #    "invalid_auth": "Invalid authentication credentials. Please check and try again.",
+        #    "unknown": "Unknown error occurred while connecting to vacuum. Please check the connection and try again."
+        #},
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_VACUUM_ENTITY_ID): cv.string,
-                    vol.Required(CONF_VACUUM_CONNECTION_STRING): cv.string,
-                    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-                }
-            ),
-            errors=errors,
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
