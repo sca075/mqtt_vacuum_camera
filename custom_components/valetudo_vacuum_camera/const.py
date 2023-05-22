b9c4f7d8e31b36c353177f32fb02e0cacb950e42
SCA075 <82227818+sca075@users.noreply.github.com>
2023-05-22 22:05:18 +0300

    const.py added the MQTT config entries.
    Signed-off-by: SCA075 <82227818+sca075@users.noreply.github.com>

--- custom_components/valetudo_vacuum_camera/const.py
+++ custom_components/valetudo_vacuum_camera/const.py
@@ -6,6 +6,8 @@ DOMAIN = "valetudo_vacuum_camera"
 DEFAULT_NAME = "valetudo vacuum camera"
 CONF_VACUUM_CONNECTION_STRING = "vacuum_map"
 CONF_VACUUM_ENTITY_ID = "vacuum_entity"
+CONF_MQTT_USER = "MQTT_User"
+CONF_MQTT_PASS = "MQTT_Pass"
 ICON = "mdi:camera"
 
 """App Constants"""
