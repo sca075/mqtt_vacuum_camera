3c3e94823798c18531e78ae5a145da177f58d5d9
SCA075 <82227818+sca075@users.noreply.github.com>
2023-05-25 17:47:38 +0300

    connector.py.py adding hass on the init for valetudo_jdata.py Signed-off-by: SCA075 <82227818+sca075@users.noreply.github.com>
    Signed-off-by: SCA075 <82227818+sca075@users.noreply.github.com>

--- custom_components/valetudo_vacuum_camera/valetudo/connector.py
+++ custom_components/valetudo_vacuum_camera/valetudo/connector.py
@@ -1,11 +1,13 @@
 import logging
 import paho.mqtt.client as mqtt
 
+from custom_components.valetudo_vacuum_camera.utils.valetudo_jdata import RawToJson
+
 _LOGGER = logging.getLogger(__name__)
 
 
 class ValetudoConnector:
-    def __init__(self, mqtt_topic):
+    def __init__(self, mqtt_topic, hass):
         self._mqtt_topic = mqtt_topic
         self._broker = "127.0.0.1"
         self._payload = None
@@ -16,16 +18,19 @@ class ValetudoConnector:
         self._mqtt.connect_async(host=self._broker)
         self._mqtt.enable_bridge_mode()
         self._mqtt.loop_start()
+        self._img_decoder = RawToJson(hass)
 
-    def update_data(self, data):
-        if data:
+    def update_data(self):
+        if self._payload:
+            self._payload = self._img_decoder.camera_message_received(self._payload)
             return self._payload
         else:
             return None
 
     def on_message(self, client, userdata, msg):
-        _LOGGER.debug("Received data from MQTT: %s", msg.payload)
         self._payload = msg.payload
+        _LOGGER.debug("Received data from MQTT: %s", self._payload)
+
 
     def on_connect(self, client, userdata, flags, rc):
         _LOGGER.debug("Connected to MQTT broker.")
