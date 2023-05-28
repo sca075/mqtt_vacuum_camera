7122eb99fcf6cfb85c91595aba4c16d893dbe01f
SCA075 <82227818+sca075@users.noreply.github.com>
2023-05-28 14:12:28 +0200

    camera.py test: buffer data from camera.py Signed-off-by: SCA075 <82227818+sca075@users.noreply.github.com>
    Signed-off-by: SCA075 <82227818+sca075@users.noreply.github.com>

--- custom_components/valetudo_vacuum_camera/valetudo/vacuum.py
+++ custom_components/valetudo_vacuum_camera/valetudo/vacuum.py
@@ -1,14 +1,31 @@
+import logging
+#import numpy as np
+#from custom_components.valetudo_vacuum_camera.utils.colors import color_home_background
 
+_LOGGER = logging.getLogger(__name__)
 
-class Vacuum:
-    def __init__(self, json_data):
-        self.status = None
-        self.x = None
-        self.y = None
-        self.a = None
-        self.charger_x = None
-        self.charger_y = None
+class Vacuum(object):
+    ''' Vacuum Class share the actual Vacuum status and
+        other information's grabbed during the execution '''
+    def __init__(self):
+        self._new_mqtt_message = False
+        self._last_image = None
 
-        async def get_mqtt_data(self) -> Optional[bytes]:
-            return None
+    def is_data_available(self):
+        value = self._new_mqtt_message
+        _LOGGER.debug("MQTT data available: %s", value)
+        return value
 
+    def set_new_mqtt_data_available(self, value: bool):
+        '''set_new_mqtt_data_available get on_message state'''
+        _LOGGER.debug("MQTT set data available: %s", value)
+        self._new_mqtt_message = value
+
+    def set_last_image(self, data):
+        self._last_image = data
+
+    def last_image_binary(self):
+        '''last_image_stored
+        :return get and return the last
+        binary data of the camera precessed image'''
+        return self._last_image
