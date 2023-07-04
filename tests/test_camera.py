10a4e4e7112a3cbd082b47be52e85d26c2d2545c
SCA075 <82227818+sca075@users.noreply.github.com>
2023-07-04 13:34:26 +0200

    Update test_camera.py

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -4,7 +4,7 @@ from tests.testsupport.broker import fake_websocket_broker, fake_broker
 import pytest
 import socket
 
-from custom_components.valetudo_vacuum_camera.camera import ValetudoCamera
+from valetudo_vacuum_camera.camera import ValetudoCamera
 from homeassistant.components.camera import Camera
 
 
