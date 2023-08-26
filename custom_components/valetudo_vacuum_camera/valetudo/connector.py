4ede2f9f36f82b30b91e981cc901ffcdf443f003
Rohan Kapoor <rohan@rohankapoor.com>
2023-08-26 00:57:26 -0700

    Use Home Assistant's MQTT infrastructure instead of rolling our own

--- custom_components/valetudo_vacuum_camera/valetudo/connector.py
+++ custom_components/valetudo_vacuum_camera/valetudo/connector.py
@@ -1,32 +1,18 @@
 """Version 1.3.4"""
 import logging
-import time
-import paho.mqtt.client as client
+from homeassistant.core import callback
+from homeassistant.components import mqtt
 from custom_components.valetudo_vacuum_camera.utils.valetudo_jdata import RawToJson
 
 _LOGGER = logging.getLogger(__name__)
+_QOS = 0
 
 
-class ValetudoConnector(client.Client):
-    def __init__(self, mqtthost, mqttusr, mqttpass, mqtt_topic, hass):
-        super().__init__("valetudo_connector")
+class ValetudoConnector:
+    def __init__(self, mqtt_topic, hass):
+        self._hass = hass
         self._mqtt_topic = mqtt_topic
-        if mqtt_topic:
-            self._mqtt_subscribe = ([
-                (str(mqtt_topic + "/MapData/map-data-hass"), 0),
-                (str(mqtt_topic + "/StatusStateAttribute/status"), 0),
-                (str(mqtt_topic + "/StatusStateAttribute/error_description"), 0),
-            ])
-        self._broker = mqtthost
-        if not self._broker:
-            self._broker = "core-mosquitto"
-        self.username_pw_set(username=mqttusr, password=mqttpass)
-        self.on_connect = self.on_connect_callback
-        self.on_message = self.on_message_callback
-        self.connect_async(host=self._broker, port=1883)
-        self.enable_bridge_mode()
-        self.loop_start()
-        self._mqtt_run = False
+        self._unsubscribe_handlers = []
         self._rcv_topic = None
         self._payload = None
         self._img_payload = None
@@ -34,7 +20,6 @@ class ValetudoConnector(client.Client):
         self._mqtt_vac_err = None
         self._data_in = False
         self._img_decoder = RawToJson(hass)
-        self.is_client_check_mode(mqtt_topic)
 
     def update_data(self, process: bool = True):
         if self._img_payload:
@@ -71,8 +56,10 @@ class ValetudoConnector(client.Client):
                 file.write(self._img_payload)
             _LOGGER.info("Saved image data from MQTT in mqtt_" + file_name + ".raw!")
 
-    def on_message_callback(self, client, userdata, msg):
+    @callback
+    async def async_message_received(self, msg):
         self._rcv_topic = msg.topic
+
         if self._rcv_topic == (self._mqtt_topic + "/MapData/map-data-hass"):
             _LOGGER.debug("Received " + self._mqtt_topic + " image data from MQTT")
             self._img_payload = msg.payload
@@ -100,43 +87,19 @@ class ValetudoConnector(client.Client):
                 + " from MQTT"
             )
 
-    def on_connect_callback(self, client, userdata, flags, rc):
-        self.subscribe(self._mqtt_subscribe)
-        _LOGGER.debug("Subscribed to MQTT broker with topic: " + self._mqtt_topic)
-
-    def stop_and_disconnect(self):
-        self.loop_stop(force=False)  # Stop the MQTT loop gracefully
-        self.disconnect()  # Disconnect from the broker
-        _LOGGER.debug(self._mqtt_topic + ": Stopped and disconnected from MQTT broker.")
-
-    def connect_broker(self):
-        self.connect_async(host=self._broker, port=1883)
-        self.enable_bridge_mode()
-        self.loop_start()
-        _LOGGER.debug(self._mqtt_topic + ": Connect MQTT broker.")
-
-    def client_start(self):
-        self.loop_start()
-        _LOGGER.debug(self._mqtt_topic + ": Started MQTT loop")
-
-    def client_stop(self):
-        self.loop_stop()
-        self._mqtt_run = False
-        _LOGGER.debug(self._mqtt_topic + ": Stopped MQTT loop")
-
-    def is_client_check_mode(self, check_topic):
-        test_topic = "valetudo/myTopic"
-        if check_topic is test_topic:
-            _LOGGER.warning("Valetudo Connector test Topic ON %s", {check_topic})
-            try:
-                with open("tests/mqtt_data.raw", "rb") as file:
-                    binary_data = file.read()
-                self._img_payload = binary_data
-                self._data_in = True
-                self.update_data()
-            except FileExistsError:
-                _LOGGER.warning(
-                    "Valetudo Connector undefined Topic, please check your configuration."
+    async def async_subscribe_to_topics(self):
+        if self._mqtt_topic:
+            for x in [
+                self._mqtt_topic + "/MapData/map-data-hass",
+                self._mqtt_topic + "/StatusStateAttribute/status",
+                self._mqtt_topic + "/StatusStateAttribute/error_description",
+            ]:
+                self._unsubscribe_handlers.append(
+                    await mqtt.async_subscribe(
+                        self._hass, x, self.async_message_received, _QOS, encoding=None
+                    )
                 )
-            time.sleep(1.5)
-            self.loop_stop()
+
+    async def async_unsubscribe_from_topics(self):
+        for x in self._unsubscribe_handlers:
+            await x()
