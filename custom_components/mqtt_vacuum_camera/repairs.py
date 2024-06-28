ecd2bdd2decc66174aad73e0f6c3451b464717dd
SCA075 <82227818+sca075@users.noreply.github.com>
2024-06-28 08:48:58 +0200

    MQTT Vacuum Camera 2024.07.0
    Signed-off-by: 82227818+sca075@users.noreply.github.com <82227818+sca075@users.noreply.github.com>

---
+++ custom_components/mqtt_vacuum_camera/repairs.py
@@ -0,0 +1,39 @@
+"""Repairs for the MQTT Vacuum Camera integration."""
+
+from __future__ import annotations
+
+import voluptuous as vol
+
+from homeassistant import data_entry_flow
+from homeassistant.components.repairs import RepairsFlow
+from homeassistant.core import HomeAssistant
+
+
+class Issue1RepairFlow(RepairsFlow):
+    """Handler for an issue fixing flow."""
+
+    async def async_step_init(
+        self, user_input: dict[str, str] | None = None
+    ) -> data_entry_flow.FlowResult:
+        """Handle the first step of a fix flow."""
+
+        return await (self.async_step_confirm())
+
+    async def async_step_confirm(
+        self, user_input: dict[str, str] | None = None
+    ) -> data_entry_flow.FlowResult:
+        """Handle the confirm step of a fix flow."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data={})
+
+        return self.async_show_form(step_id="confirm", data_schema=vol.Schema({}))
+
+
+async def async_create_fix_flow(
+    hass: HomeAssistant,
+    issue_id: str,
+    data: dict[str, str | int | float | None] | None,
+) -> RepairsFlow:
+    """Create flow."""
+    if issue_id == "issue_1":
+        return Issue1RepairFlow()
