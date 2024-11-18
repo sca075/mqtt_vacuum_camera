ab1cec14ca8b19b41c814ac9666eb8eaa077e61d
SCA075 <82227818+sca075@users.noreply.github.com>
2024-11-19 00:51:05 +0100

    updated and fix camera_services.py
    Signed-off-by: 82227818+sca075@users.noreply.github.com <82227818+sca075@users.noreply.github.com>

--- custom_components/mqtt_vacuum_camera/utils/camera/camera_services.py
+++ custom_components/mqtt_vacuum_camera/utils/camera/camera_services.py
@@ -1,33 +1,34 @@
 """Camera-related services for the MQTT Vacuum Camera integration."""
 
+import logging
 import asyncio
 import async_timeout
-import logging
-
 
 from homeassistant.core import ServiceCall, HomeAssistant
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import SERVICE_RELOAD
 
 from ...utils.files_operations import async_clean_up_all_auto_crop_files
+from ...const import  DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-async def reset_trims(hass: HomeAssistant, call: ServiceCall, domain: str) -> None:
+async def reset_trims( call: ServiceCall, hass: HomeAssistant) -> None:
     """Action Reset Map Trims."""
-    _LOGGER.debug(f"Resetting trims for {domain}")
+    _LOGGER.debug(f"Resetting trims for {DOMAIN}")
     try:
         await async_clean_up_all_auto_crop_files(hass)
-        await hass.services.async_call(domain, SERVICE_RELOAD)
-        hass.bus.async_fire(f"event_{domain}_reset_trims", context=call.context)
+        await hass.services.async_call(DOMAIN, SERVICE_RELOAD)
+        hass.bus.async_fire(f"event_{DOMAIN}_reset_trims", context=call.context)
     except Exception as err:
         _LOGGER.error(f"Error resetting trims: {err}")
 
 
-async def reload_config(hass: HomeAssistant, domain: str) -> None:
+async def reload_camera_config(call: ServiceCall, hass: HomeAssistant) -> None:
     """Reload the camera platform for all entities in the integration."""
-    _LOGGER.debug(f"Reloading the config entry for all {domain} entities")
-    camera_entries = hass.config_entries.async_entries(domain)
+
+    _LOGGER.debug(f"Reloading the config entry for all {DOMAIN} entities")
+    camera_entries = hass.config_entries.async_entries(DOMAIN)
     total_entries = len(camera_entries)
     processed = 0
 
@@ -35,16 +36,12 @@ async def reload_config(hass: HomeAssistant, domain: str) -> None:
         processed += 1
         _LOGGER.info(f"Processing entry {processed}/{total_entries}")
         if camera_entry.state == ConfigEntryState.LOADED:
-            _LOGGER.debug(f"Unloading entry: {camera_entry.entry_id}")
             try:
-                async with async_timeout.timeout(30):
-                    await hass.config_entries.async_unload(camera_entry.entry_id)
-
-                _LOGGER.debug(f"Reloading entry: {camera_entry.entry_id}")
-                await hass.config_entries.async_setup(camera_entry.entry_id)
+                with async_timeout.timeout(10):
+                    _LOGGER.debug(f"Reloading entry: {camera_entry.entry_id}")
+                    hass.config_entries.async_schedule_reload(camera_entry.entry_id)
             except asyncio.TimeoutError:
-                _LOGGER.error(f"Timeout while processing entry {camera_entry.entry_id}")
-                continue
+                _LOGGER.error(f"Timeout processing entry {camera_entry.entry_id}")
             except Exception as err:
                 _LOGGER.error(f"Error processing entry {camera_entry.entry_id}: {err}")
                 continue
@@ -53,7 +50,9 @@ async def reload_config(hass: HomeAssistant, domain: str) -> None:
                 f"Skipping entry {camera_entry.entry_id} as it is NOT_LOADED"
             )
 
-    hass.bus.async_fire(f"event_{domain}_reloaded", {
-        "processed": processed,
-        "total": total_entries
-    })
+    hass.bus.async_fire(f"event_{DOMAIN}_reloaded",
+                        event_data= {
+                            "processed": processed,
+                            "total": total_entries,
+                        },
+                        context=call.context)
